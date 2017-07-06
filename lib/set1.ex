2b6bc1818ae8867720a839d1ae01a1de1623f220
Andrew Davidson <andrew@clearbluetechnologies.com>
2017-07-06 16:23:55 -0400

    Challenge 2

--- lib/set1.ex
+++ lib/set1.ex
@@ -1,5 +1,17 @@
 defmodule Set1 do
-	def hex_to_base64(hex) do
-		hex |> String.upcase |> Base.decode16! |> Base.encode64
+	require Bitwise
+
+	# challenge 1
+	def hex_to_base64(str) do
+		str |> String.upcase |> Base.decode16! |> Base.encode64
 	end
+
+	# challenge 2
+	def fixed_xor(str1, str2) do
+		l1 = str1 |> String.upcase |> Base.decode16! |> :binary.bin_to_list
+		l2 = str2 |> String.upcase |> Base.decode16! |> :binary.bin_to_list
+		Enum.zip(l1, l2) |> Enum.map(fn({x,y}) -> Bitwise.bxor(x, y) end) |> :binary.list_to_bin |> Base.encode16 |> String.downcase
+	end
+
+	# challenge 3
 end
--- test/set1_test.exs
+++ test/set1_test.exs
@@ -2,9 +2,16 @@ defmodule Set1Test do
   use ExUnit.Case
   doctest Set1
 
-  test "converts hex string to base64" do
-  	hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-  	base64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+  test "Challenge 1" do
+	hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+	base64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
     assert Set1.hex_to_base64(hex) == base64
+  end
+
+  test "Challenge 2" do
+	hex1 = "1c0111001f010100061a024b53535009181c"
+	hex2 = "686974207468652062756c6c277320657965"
+	xord = "746865206b696420646f6e277420706c6179"
+	assert Set1.fixed_xor(hex1, hex2) == xord
   end
 end
