0014ef50d37bf5baabfea97156f042469e06b129
Andrew Davidson <andrew@clearbluetechnologies.com>
2017-07-23 16:01:21 -0400

    Challenge 9

--- lib/crypto.ex
+++ lib/crypto.ex
@@ -4,5 +4,9 @@ defmodule Crypto do
 		:crypto.block_decrypt(:aes_ecb, key, ct) |> String.replace(~r/\x00|\x04/, "")
 	end
 
-
+	def pad_pkcs_7(block, len) do
+		true = byte_size(block) < len
+		pad_length = len - byte_size(block)
+		block <> :binary.copy(<<pad_length>>, pad_length)
+	end
 end
