2c177337081abc2571fbdc66d467298325f321c1
Andrew Davidson <andrew@davidson.pw>
2024-08-03 19:14:26 -0400

    set 1 challenge 8

--- aes.rb
+++ aes.rb
@@ -1,9 +1,19 @@
 require 'openssl'
 module AES
+  BLOCK_SIZE = 16
   def self.decrypt_ecb ciphertext, key
     cipher = OpenSSL::Cipher::AES128.new(:ECB)
     cipher.decrypt
     cipher.key = key
     cipher.update(ciphertext) + cipher.final
+  end
+
+  # assume file of newline delimited hex encoded strings
+  def self.detect_aes_ecb filename
+    file =File.read filename
+    ciphertexts = file.split("\n").map {|hex| [hex].pack("H*")}
+    ciphertexts.max_by do |ct|
+      ct.scan(/.{1,#{BLOCK_SIZE}}/).tally.values.max
+    end
   end
 end
